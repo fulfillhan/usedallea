602d4053c4f9fe485a7c2cc244fbe48b7d017389
unnine <134513528+unnine@users.noreply.github.com>
2024-03-31 18:00:31 +0900

    Merge pull request #4 from fulfillhan/feature/style
    상품 상세보기 화면 퍼블리싱

--- src/main/java/com/application/usedallea/PageController.java
+++ src/main/java/com/application/usedallea/PageController.java
@@ -19,7 +19,7 @@ public class PageController {
 
     @RequestMapping("/product/detail")
     public String registerDetail() {
-        return "productDetailByUser";
+        return "product/productDetail";
     }
 
 }
