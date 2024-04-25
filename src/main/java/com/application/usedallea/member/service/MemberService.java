cdf272829e2559e6ab187fe90f5d73d7b7444a8d
sanghui <tkdgml719@gamil.com>
2024-04-25 17:02:28 +0900

    [완료] registerOrUpdate 체크박스 부분 수정.

--- src/main/java/com/application/usedallea/member/service/MemberService.java
+++ src/main/java/com/application/usedallea/member/service/MemberService.java
@@ -4,15 +4,15 @@ import com.application.usedallea.member.dto.MemberDTO;
 
 public interface MemberService {
 
-	public void registerMember(MemberDTO memberDTO);
+	void registerMember(MemberDTO memberDTO);
 
-	public String dupleCheckId(String userId);
+	String dupleCheckId(String userId);
 
-	public boolean login(MemberDTO memberDTO);
+	boolean login(MemberDTO memberDTO);
 
-     public MemberDTO getMemberDetail(String userId);
+	MemberDTO getMemberDetail(String userId);
 
-	 public void updateMember(MemberDTO memberDTO);
+	void updateMember(MemberDTO memberDTO);
 
-	 public void updateDelete(String userId);
+	void updateDelete(String userId);
 }
